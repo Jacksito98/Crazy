e3d6c2b6b5e891fa43b5d2d1a7cff1bd470fc1c9
Jack andy <80754943+Jacksito98@users.noreply.github.com>
2021-04-07 04:07:26 -0400

    Add files via upload

---
+++ src/help1.js
@@ -0,0 +1,36 @@
+const help1 = (prefix) => {
+
+	return `
+┣━━━━°❀ ❬ *𝐶𝑅𝐸𝐴𝑅* ❭ ❀°━━━━┓
+┃
+┣⊱❥ *xd*  (𝑥𝑑)
+┣⊱❥ *pinterest* (𝑏𝑢𝑠𝑐𝑎𝑟 𝑦 𝑒𝑛𝑣𝑖𝑎𝑟 𝑖𝑚𝑎𝑔𝑒𝑛 𝑑𝑒 𝑝𝑖𝑛𝑠𝑡𝑒𝑟𝑒𝑠𝑡)
+┣⊱❥ *anjing* (𝑛𝑜 𝑠𝑒)
+┣⊱❥ *loli2* (𝑙𝑜𝑙𝑖)
+┣⊱❥ *anime* (𝑒𝑛𝑣𝑖𝑎𝑒 𝑓𝑜𝑡𝑜 𝑑𝑒 𝑎𝑛𝑖𝑚𝑒)
+┣⊱❥ *wolflogo2 <texto | texto>* (𝑙𝑜𝑔𝑜 𝑑𝑒 𝑙𝑜𝑏𝑜𝑠)
+┣⊱❥ *quotemaker <tx | wtrmk | tema>* (𝑒𝑑𝑖𝑡𝑜𝑟(𝑖𝑚𝑎𝑔𝑒𝑛))
+┣⊱❥ *galaxtext* (𝑡𝑒𝑥𝑡𝑜 𝑒𝑛 𝑓𝑜𝑛𝑑𝑜 𝑑𝑒 𝑔𝑎𝑙𝑎𝑥𝑖𝑎)
+┣⊱❥ *textdark* (𝑡𝑒𝑥𝑡𝑜 𝑜𝑐𝑠𝑐𝑢𝑟𝑜)
+┣⊱❥ *textblue* (𝑡𝑒𝑥𝑡𝑜 𝑎𝑧𝑢𝑙?)
+┣⊱❥ *lovemake* (𝑡𝑒𝑥𝑡𝑜 𝑑𝑒 𝑎𝑚𝑜𝑟)
+┣⊱❥ *stiltext* (𝑛𝑜 𝑟𝑒𝑐𝑢𝑒𝑟𝑑𝑜 ;-;)
+┣⊱❥ *ninjalogo* (𝑙𝑜𝑔𝑜 𝑛𝑖𝑛𝑗𝑎)
+┣⊱❥ *party* (𝑓𝑖𝑒𝑠𝑡𝑎)
+┣⊱❥ *rtext* (𝑛𝑜)
+┣⊱❥ *water* (𝑎𝑔𝑢𝑎)
+┣⊱❥ *lionlogo <texto | texto>* (𝑙𝑜𝑔𝑜 𝑙𝑒𝑜𝑛?)
+┣⊱❥ *textscreen* (𝑡𝑒𝑠𝑡)
+┣⊱❥ *text3d* (ℎ𝑎𝑐𝑒𝑟 𝑡𝑒𝑥𝑡𝑜 3𝐷)
+┣⊱❥ *epep* (𝑡𝑒𝑠𝑡)
+┣⊱❥ *marvelogo <texto | texto>* (𝑙𝑜𝑔𝑜 𝑑𝑒 𝑚𝑎𝑟𝑣𝑒𝑙)
+┣⊱❥ *snow <texto | texto>* (𝑙𝑜𝑔𝑜 𝑛𝑖𝑒𝑏𝑒)
+┣⊱❥ *firetext* (𝑙𝑜𝑔𝑜 𝑑𝑒 𝑓𝑢𝑒𝑔𝑜)
+
+════════════════════
+*𝙹𝙰𝙲𝙺𝚂𝙸𝚃𝙾 𝐵𝑂𝑇* 🤗
+*𝑈𝑤𝑈*
+════════════════════`
+}
+exports.help1 = help1
+
