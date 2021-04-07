a56b01e07d042ceb3afa9be1fb785dce78604497
Jack andy <80754943+Jacksito98@users.noreply.github.com>
2021-04-07 04:02:13 -0400

    Add files via upload

---
+++ src/iklan.js
@@ -0,0 +1,19 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 𝐵𝑂𝑇 𝐸𝑋𝐶𝐿𝑈𝑆𝐼𝑉𝑂 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA RENTA Y BOTS PRIVADOS:*
+╠➥ *EL PRECIO ES EN DOLARES*
+╠➥ *RENTA: $6 / GRUPO (MES)*
+╠➥ *CREAR: $12 (TIENES TU PROPIO BOT Y PUEDES CONTROLARLO)*
+╠➥ *METODOS DE PAGO 👇*
+╠➥ *PAY PAL, CUENTAS DE FREE FIRE*
+╠═══════════════════════════
+╠➥ *VENTAS*
+╠➥ *wa.me/18295176783*
+║
+╚═〘  𝐋𝐔𝐂𝐈𝐅𝐄𝐑-𝐁𝐎𝐓  〙
+`
+}
+exports.iklan = iklan
