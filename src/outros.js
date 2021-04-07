da33049403da0b4289297a974a79415b39a7a782
Jack andy <80754943+Jacksito98@users.noreply.github.com>
2021-04-07 04:00:33 -0400

    Add files via upload

---
+++ src/outros.js
@@ -0,0 +1,41 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 *𝑀𝐸𝑁𝑈 𝐷𝐸 𝑂𝑇𝑅𝑂𝑆* 〙✪══
+║
+╠➥ *${prefix}linkgp* (𝑉𝑒𝑟 𝑙𝑖𝑛𝑘 𝑑𝑒𝑙 𝑔𝑟𝑢𝑝𝑜)
+╠➥ *${prefix}simih [1/0]* (𝑎𝑐𝑡𝑖𝑣𝑎𝑟 𝑖𝑛𝑡𝑒𝑙𝑖𝑔𝑒𝑛𝑐𝑖𝑎 𝑎𝑟𝑡𝑖𝑓𝑖𝑐𝑖𝑎𝑙)
+╠➥ *${prefix}marcar* (𝑚𝑎𝑟𝑐𝑎𝑟)
+╠➥ *${prefix}add [@]* (𝐴𝑛̃𝑎𝑑𝑖𝑟 𝑚𝑖𝑒𝑚𝑏𝑟𝑜)
+╠➥ *${prefix}banir [@]* (𝑒𝑥𝑝𝑢𝑙𝑠𝑎𝑟 𝑎 𝑎𝑙𝑔𝑢𝑖𝑒𝑛)
+╠➥ *${prefix}promover [@]* (𝑑𝑎𝑟 𝑎𝑑𝑚𝑖𝑛)
+╠➥ *${prefix}rebaixar*(𝑞𝑢𝑖𝑡𝑎𝑟 𝑎𝑑𝑚𝑖𝑛)
+╠➥ *${prefix}admins* (𝑣𝑒𝑟 𝑙𝑜𝑠 𝑎𝑑𝑚𝑖𝑛𝑠 𝑑𝑒𝑙 𝑔𝑟𝑢𝑝𝑜)
+╠➥ *${prefix}marcar2* (𝑚𝑒𝑛𝑐𝑖𝑜𝑛𝑎𝑟 𝑎 𝑡𝑜𝑑𝑜)
+╠➥ *${prefix}bc [texto]* (ℎ𝑎𝑐𝑒𝑟 𝑎𝑛𝑢𝑛𝑐𝑖𝑜)
+╠➥ *${prefix}marcar3* (𝑚𝑎𝑟𝑐𝑎𝑟 𝑎 𝑡𝑜𝑑𝑜𝑠)
+╠➥ *${prefix}bloqueados* (𝑣𝑒𝑟 𝑙𝑖𝑠𝑡𝑎 𝑑𝑒 𝑏𝑙𝑜𝑞𝑢𝑒𝑎𝑑𝑜𝑠)
+╠➥ *${prefix}bloquear [@]* (𝑏𝑙𝑜𝑞𝑢𝑒𝑎𝑟 𝑢𝑠𝑢𝑎𝑟𝑖𝑜)
+╠➥ *${prefix}desbloquear [@]* (𝑑𝑒𝑠𝑏𝑙𝑜𝑞𝑢𝑒𝑎𝑟 𝑢𝑠𝑢𝑎𝑟𝑖𝑜)
+╠➥ *${prefix}limpar* (𝑙𝑖𝑚𝑝𝑖𝑎𝑟)
+╠➥ *${prefix}bc [ *texto* ]* (𝑏𝑟𝑜𝑎𝑑𝑐𝑎𝑠𝑡)
+╠➥ *${prefix}bemvindo [1/0]* (𝑎𝑐𝑡𝑖𝑣𝑎𝑟 𝑏𝑖𝑒𝑛𝑣𝑒𝑛𝑖𝑑𝑎𝑠 𝑒𝑛 𝑒𝑠𝑡𝑒 𝑔𝑟𝑢𝑝𝑜)
+╠➥ *${prefix}clonar [@]* (𝑐𝑙𝑜𝑛𝑎𝑟 𝑝𝑒𝑟𝑓𝑖𝑙)
+╠➥ *${prefix}help1* (𝑎𝑏𝑟𝑖𝑟 𝑒𝑙 𝑠𝑒𝑔𝑢𝑛𝑑𝑜 𝑚𝑒𝑛𝑢)
+╠➥ *${prefix}dono* (𝑐𝑟𝑒𝑎𝑑𝑜𝑟)
+╠➥ *${prefix}owner* (𝑐𝑟𝑒𝑎𝑑𝑜𝑟)
+╠➥ *${prefix}tts [texto]* (𝑡𝑒𝑥𝑡𝑜 𝑎 𝑣𝑜𝑧)
+╠➥ *${prefix}setnome* (𝑐𝑎𝑚𝑏𝑖𝑎𝑟 𝑛𝑜𝑚𝑏𝑟𝑒)
+╠➥ *${prefix}termux* (𝑡𝑒𝑟𝑚𝑢𝑥)
+╠➥ *${prefix}setfoto* (𝑐𝑎𝑚𝑏𝑖𝑎𝑟 𝑓𝑜𝑡𝑜)
+╠➥ *${prefix}grupoinfo* (𝑣𝑒𝑟 𝑖𝑛𝑓𝑜 𝑑𝑒𝑙 𝑔𝑟𝑢𝑝𝑜)
+╠➥ *${prefix}ytmp4* (𝑑𝑒𝑠𝑐𝑎𝑟𝑔𝑎𝑟 𝑣𝑖𝑑𝑒𝑜 𝑒𝑛 𝑓𝑜𝑟𝑚𝑎𝑡𝑜 𝑚𝑝4)
+╠➥ *${prefix}bomdia* (𝑏𝑢𝑒𝑛 𝑑𝑖𝑎)
+╠➥ *${prefix}boanoite* (𝑏𝑢𝑒𝑛𝑎𝑠 𝑛𝑜𝑐ℎ𝑒𝑠)
+╠➸ *${prefix}marcar* (𝑚𝑒𝑛𝑐𝑖𝑜𝑛𝑎𝑟 𝑎 𝑡𝑜𝑑𝑜𝑠)
+╠➸ *${prefix}marcar2* (𝑚𝑒𝑛𝑐𝑖𝑜𝑛𝑎𝑟 𝑎 𝑡𝑜𝑑𝑜𝑠)
+╠➸ *${prefix}marcar3* (𝑚𝑒𝑛𝑐𝑖𝑜𝑛𝑎𝑟 𝑎 𝑡𝑜𝑑𝑜𝑠)
+║
+╚═〘  *𝐋𝐔𝐂𝐈𝐅𝐄𝐑-𝐁𝐎𝐓* 〙
+`
+}
+exports.iklan = iklan
