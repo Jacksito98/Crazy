ffd3f8b7bb05a5c482a81a95fb9e9726114742bf
Jack andy <80754943+Jacksito98@users.noreply.github.com>
2021-04-07 03:55:45 -0400

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PRECIO DE LISTA DE VIP :*
+
+-Dolares. 2 > Acceder a los recursos ViP
+-Dolares. 10 > Recursos VIP + ¡Inserta el bot en tu grupo!
+
+*SI QUIERES REGISTRARTE VIP :*
+
+*PPropietario del chat BOT :*
+
+_wa.me/18295176783 o escriba *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DE CAUSANTES ☿🚩 :*
+_https://chat.whatsapp.com/K9EGMOGyHXO7M2yCKrBlyA `
+}
+exports.daftarvip = daftarvip
