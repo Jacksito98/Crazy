396871a8b3b06266d774602339ede4948e946c7f
Jack andy <80754943+Jacksito98@users.noreply.github.com>
2021-04-07 03:59:50 -0400

    Add files via upload

---
+++ src/pack.js
@@ -0,0 +1,19 @@
+const pack = (prefix) => {
+    return `*PACKS:*
+    
+*-𝐁𝐎𝐓-V6 ☿🚩*
+
+Belle delphine : https://mega.nz/folder/mHhRAaZI#PWz-yNNldK4Goibk_lNFaw 
+
+Pack1: https://mega.nz/#F!QzAX0Q6R!LGZhx56Dv9kO2uN9ykngiA!srA1WKaK
+
+Kitty kum: https://photos.app.goo.gl/gNxLbJHGVzeWY9iS9 
+
+pack 3:https://mega.nz/folder/sM8xUKxI#TDK3cbpJwl97th-CIs7pmw 
+
+pack 4: https://mega.nz/folder/iZhUDajS#gL0Rcaz0yvVVtMzgHqQLZw
+
+pack 5: https://mega.nz/folder/gg0ixDqY#qVVo5qrKrsZmbIU8JHREaA 
+`
+}
+exports.pack = pack
