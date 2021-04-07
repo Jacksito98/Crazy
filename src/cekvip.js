8c70984ac1f3017b69e0dc14eb108cbb4e53eda9
Jack andy <80754943+Jacksito98@users.noreply.github.com>
2021-04-07 04:03:34 -0400

    Add files via upload

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nombre del bot* :  Kill-BOT
+──────────────────
+        『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*USTED ES UN USUARIO PREMIUM* ☿🚩`
+}
+exports.cekvip = cekvip
